0571c81d0be940d35a3d85d9a88169104c01a3b7
jagallob <jagallob@eafit.edu.co>
2025-03-14 20:40:34 -0500

    Primer commit: Proyecto Calculadora React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Calculadora from "./components/Calculadora";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <Calculadora />
+    </div>
+  );
+}
+
+export default App;
